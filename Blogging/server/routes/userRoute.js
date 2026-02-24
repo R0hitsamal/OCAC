1a1036c28bba587737306f8aa80f7d6ec9a3037e
R0hitsamal <samalrohit2567@gmail.com>
2026-02-24 22:20:59 +0530

    First commit

--- Blogging/server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const Register = require("../controller/user")
-const router =  express.Router()
-
-router.post("/register",Register)
-
-module.exports =  router;
